6bfac52f1c973f63279f28dbda6b107ca10a4c3e
amirsssss <amir_matics2008@yahoo.com>
2018-11-25 20:25:18 +0330

    Added Templates

--- generator/views.py
+++ generator/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import GeneratorModel
 # Create your views here.
+
+
+
+def LastData(request,*args,**kwargs):
+    data=GeneratorModel.objects.all()
+    # print(data)
+    # if len(data)>1:
+    #     lastdata=data[len(data)-1]
+    # else:
+    #     lastdata=-1
+    return render(request,"generator/visu.html",context={'data':data})
--- project/urls.py
+++ project/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from generator.views import LastData
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',LastData)
 ]
